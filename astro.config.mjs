e8ea7865b1b86229186acef09972a90fa77e5cd2
J4N <142495328+J4NBI@users.noreply.github.com>
2025-01-29 11:03:24 +0100

    in progress working doc section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import starlight from '@astrojs/starlight';
+
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [starlight({
+    title: 'Meine Dokumentation',
+  }),, tailwind({
+    applyBaseStyles: false,
+  }), react()],
+});
